acdd681585ad69fc9050209e6d116cc40a956628
Randy <randytia.akbar@gmail.com>
2020-10-11 20:13:43 +0700

    general page

---
+++ lib/shared/theme.dart
@@ -0,0 +1,8 @@
+part of 'shared.dart';
+
+Color greyColor = '8D92A3'.toColor();
+Color mainColor = 'FFC700'.toColor();
+
+TextStyle greyFontSytle = GoogleFonts.poppins().copyWith(color: greyColor);
+
+const double defaultMargin = 24;
---
+++ lib/ui/pages/general_page.dart
@@ -0,0 +1,86 @@
+part of 'pages.dart';
+
+class GeneralPage extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Function onBackButtonPressed;
+  final Widget child;
+  final Color backColor;
+
+  GeneralPage(
+      {this.title = 'Title',
+      this.subtitle = 'subtitle',
+      this.onBackButtonPressed,
+      this.backColor,
+      this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+          ),
+          SafeArea(
+            child: Container(
+              color: backColor ?? 'FAFAFC'.toColor(),
+            ),
+          ),
+          SafeArea(
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: defaultMargin),
+                      padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                      width: double.infinity,
+                      height: 100,
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          onBackButtonPressed != null
+                              ? Container(
+                                  width: 24,
+                                  height: 24,
+                                  margin: EdgeInsets.only(right: 26),
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image:
+                                          AssetImage('assets/back_arrow.png'),
+                                    ),
+                                  ),
+                                )
+                              : SizedBox(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                title,
+                                style: GoogleFonts.poppins(
+                                    fontSize: 22, fontWeight: FontWeight.w500),
+                              ),
+                              Text(
+                                subtitle,
+                                style: GoogleFonts.poppins(
+                                    color: '8D92A3'.toColor(),
+                                    fontWeight: FontWeight.w300),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    child ?? SizedBox()
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
