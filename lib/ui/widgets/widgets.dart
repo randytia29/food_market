5ef4efa55c29659e75646224cc570e00afe5c571
randytia29 <randytia.akbar@gmail.com>
2020-10-19 19:43:34 +0700

    custom tabbar

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:food_market/models/models.dart';
 import 'package:food_market/shared/shared.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:supercharged/supercharged.dart';
 
 part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
+part 'custom_tabbar.dart';
