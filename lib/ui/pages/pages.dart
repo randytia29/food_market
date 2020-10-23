843f10edda1d0139dc799ec7185a1750d00d1c74
randytia29 <randytia.akbar@gmail.com>
2020-10-23 19:40:23 +0700

    food detail page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -7,6 +7,7 @@ import 'package:food_market/shared/shared.dart';
 import 'package:food_market/ui/widgets/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:supercharged/supercharged.dart';
 
 part 'general_page.dart';
